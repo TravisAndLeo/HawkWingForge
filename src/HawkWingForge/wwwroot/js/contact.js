cf1ae4b7ce779ad76d681ec16ce163e992e7c11a
LeoTanoue <leo.tanoue@gmail.com>
2017-03-19 18:36:13 -0500

    Creating all the code for the Contact page. This code will associate the form with a excel sheet and email account.

---
+++ src/HawkWingForge/wwwroot/js/contact.js
@@ -0,0 +1,42 @@
+﻿// Source: https://github.com/dwyl/html-form-send-email-via-google-script-without-server
+
+// get all data in form and return object
+function getFormData() {
+    var data = {};
+    data.email = document.getElementById("email").value;
+    data.message = document.getElementById("message").value;
+    data.name = document.getElementById("name").value;
+    console.log(data);
+    return data;
+}
+
+function handleFormSubmit(event) {  // handles form submit withtout any jquery
+    event.preventDefault();           // we are submitting via xhr below
+    var data = getFormData();         // get the values submitted in the form
+    var url = event.target.action;  
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function () {
+        console.log(xhr.status, xhr.statusText)
+        console.log(xhr.responseText);
+        document.getElementById('gform').style.display = 'none'; // hide form
+        document.getElementById('thankyouMessage').style.display = 'block';
+        return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function (k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    xhr.send(encoded);
+}
+
+function loaded() {
+    console.log('contact form submission handler loaded successfully');
+    // bind to the submit event of our form
+    var form = document.getElementById('gform');
+    form.addEventListener("submit", handleFormSubmit, false);
+};
+
+document.addEventListener('DOMContentLoaded', loaded, false);
